fb7f41ee0d28a3be8f6d781b5095132e30e9ee12
Beka Haile <BHaile@excellerentsolutions.com>
2021-08-19 13:01:27 +0300

    Initial commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './search-box.styles.css'
+
+export const SearchBox = ({placeholder, search}) => {
+   return (
+   <input
+          type="search"
+          className="search"
+          placeholder={ placeholder }
+          onChange={ search } />
+   );
+}
